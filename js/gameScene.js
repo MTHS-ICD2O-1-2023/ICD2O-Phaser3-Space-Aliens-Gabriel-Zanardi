cbea4c4f9893651ab5700fc68bab176efe53fc56
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-05-03 18:19:01 +0000

    fixed typo in Ln 28

--- js/gameScene.js
+++ js/gameScene.js
@@ -25,7 +25,7 @@ class GameScene extends Phaser.Scene {
    * Can be defined on your own Scenes.
    * This method is called by the Scene Manager when the scene starts,
    * before preload() and create().
-   * @param {object} data - Any dara passed via ScenePlugin.add() or ScenePlugin.start()
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
    */
   init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
